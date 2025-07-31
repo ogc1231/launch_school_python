eba1b6b2f52d94675e3b5f1f4d4b876beaecf3d6
Oliver Craigie <ogc@live.ie>
2025-07-31 13:38:32 +0100

    Complete data types questions

---
+++ introduction_to_programming_with_python/data-types/q1.py
@@ -0,0 +1,11 @@
+# Identify the data type or class
+'True'         # str String
+False          # bool Boolean
+(1, 2, 3)      # tuple Tuple
+1.5            # float Float
+[1, 2, 3]      # list List
+2              # int Integer
+range(5)       # range Range
+{1, 2, 3}      # set Set
+None           # NoneType
+{'foo': 'bar'} # dict Dictionary
---
+++ introduction_to_programming_with_python/data-types/q2.py
@@ -0,0 +1,8 @@
+# Create a tuple called names that contains several pet names
+
+names = (
+  'Bob',
+  'Sam',
+  'Ben',
+  'Nat',
+)
