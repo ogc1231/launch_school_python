2fb935b41c0b8c22d51f04b19eb14008b7510a92
Oliver Craigie <ogc@live.ie>
2025-08-08 17:58:10 +0100

    complete py101 small problems easy2 q1-4

---
+++ py101_py109/py101_py109_small_problems/easy2/q1.py
@@ -0,0 +1,13 @@
+def greetings(list, dict):
+    name = ' '.join(list)
+    occupation = ' '.join(dict.values())
+
+    return f'Hello, {name}! Nice to have a {occupation} around.'
+
+
+greeting = greetings(
+    ["John", "Q", "Doe"],
+    {"title": "Master", "occupation": "Plumber"},
+)
+print(greeting)
+# Hello, John Q Doe! Nice to have a Master Plumber around.
---
+++ py101_py109/py101_py109_small_problems/easy2/q4.py
@@ -0,0 +1,8 @@
+def square(num):
+    return multiply(num, num)
+
+def multiply(num1, num2):
+    return num1 * num2
+
+print(square(5) == 25)   # True
+print(square(-8) == 64)  # True
