b43b8a875da3b1e778919d8bf5c62ab9f18569ed
Oliver Craigie <ogc@live.ie>
2025-08-09 19:09:14 +0100

    complete rock paper scissors game

---
+++ py101_py109/lesson2_small_programs/rock_paper_scissors/rps.py
@@ -0,0 +1,42 @@
+import random
+
+hand_list = ['rock', 'paper', 'scissors']
+
+print('Welcome to RPS')
+while True:
+    user_hand = input(f'Choose {', '.join(hand_list)}: ')
+
+    # validate user hand
+    while True:
+        if user_hand not in hand_list:
+            user_hand = input('Invalid: choose rock, paper or scissors: ')
+        else:
+            break
+
+    computer_hand = hand_list[random.randint(0, len(hand_list) - 1)]
+
+    print(f'User chose: {user_hand}')
+    print(f'Computer chose: {computer_hand}')
+
+    if (
+        (user_hand == 'rock' and computer_hand == 'scissors')
+        or (user_hand == 'scissors' and computer_hand == 'paper')
+        or  (user_hand == 'paper' and computer_hand == 'rock')
+    ):
+        print(f'User wins: {user_hand} beats {computer_hand}')
+    elif user_hand == computer_hand:
+        print(f'Draw: {user_hand} is the same as {computer_hand}')
+    else:
+        print(f'Computer wins: {computer_hand} beats {user_hand}')
+
+    ans = input('Play again? y/n ').lower()
+
+    while True:
+        if ans.startswith('n') or ans.startswith('y'):
+            break
+        print('Please enter "y" or "n".')
+        ans = input().lower()
+    
+    if ans == 'n':
+        print('Thanks for playing')
+        break
